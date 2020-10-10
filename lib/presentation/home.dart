99ca145d2eec8322b85085a1c9fb4a06895b45c0
san-smith <san-smith@mail.ru>
2020-10-10 20:06:41 +0700

    Add draft of Home screen

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habr_flutter_clean_arch/domain/model/day.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +7,77 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _latController = TextEditingController();
+  final _lngController = TextEditingController();
+
+  Day _day;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GestureDetector(
+      onTap: FocusScope.of(context).unfocus,
+      child: Scaffold(
+        body: _getBody(),
+      ),
+    );
+  }
+
+  Widget _getBody() {
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _getRowInput(),
+            SizedBox(height: 20),
+            RaisedButton(
+              child: Text('Получить'),
+              onPressed: _getDay,
+            ),
+            SizedBox(height: 20),
+            if (_day != null) _getDayInfo(_day),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _getRowInput() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _latController,
+            keyboardType: TextInputType.numberWithOptions(decimal: true, signed: true),
+            decoration: InputDecoration(hintText: 'Широта'),
+          ),
+        ),
+        SizedBox(width: 20),
+        Expanded(
+          child: TextField(
+            controller: _lngController,
+            keyboardType: TextInputType.numberWithOptions(decimal: true, signed: true),
+            decoration: InputDecoration(hintText: 'Долгота'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _getDayInfo(Day day) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text('Восход: ${day.sunrise.toLocal()}'),
+        Text('Заход: ${day.sunset.toLocal()}'),
+        Text('Полдень: ${day.solarNoon.toLocal()}'),
+        Text('Продолжительность: ${Duration(seconds: day.dayLength)}'),
+      ],
+    );
+  }
+
+  void _getDay() {
+    // здесь получаем данные
   }
 }
